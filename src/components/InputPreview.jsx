0ca1a5ae4ba0b15b9c60154536bb1b99f91b2cd8
Dante Del Favero <dante.delfavero@gmail.com>
2018-02-18 19:40:11 +0100

    flushed

--- src/components/InputPreview.jsx
+++ src/components/InputPreview.jsx
@@ -4,7 +4,11 @@ export default class InputPreview extends React.Component {
     render() {
         return(
             <div>
-                <input type="text" />
+                <input
+                    type="text"
+                    value = {this.props.value} 
+                    onChange={e => this.props.onChange(e.target.value)}
+                    />
             </div>
         );
     }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,12 +4,23 @@ import InputPreview from './../components/InputPreview';
 
 import {connect} from 'react-redux';
 
+import {setMessage} from './../actions/messages';
+
 export default class App extends Component {
+
+    _onChange = (value) => {
+        this.props.dispatch(setMessage(value))
+    }
+
     render() {
+        const {message} = this.props.messageReducer;
+
         return (
             <div>
                 <h1>Here is my react App</h1>
-                <InputPreview />
+                <InputPreview 
+                    value={message} 
+                    onChange={this._onChange}/>
             </div>
         );
     }
